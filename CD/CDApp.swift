5e91f980c7ae9df2d33ec2de576dc6d31850a89a
Paulina <paulisiaxd00@gmail.com>
2024-08-27 15:55:20 +1000

    Initial Commit

---
+++ CD/CDApp.swift
@@ -0,0 +1,17 @@
+//
+//  CDApp.swift
+//  CD
+//
+//  Created by Paulina Grabowska on 27/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct CDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CD
+//
+//  Created by Paulina Grabowska on 27/8/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
