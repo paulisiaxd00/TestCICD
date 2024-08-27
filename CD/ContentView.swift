84a0096b4ff73834cbf115778b4e8a1d1d9f4fb8
paulisiaxd00 <64190654+paulisiaxd00@users.noreply.github.com>
2024-08-27 16:16:59 +1000

    naw again (#2)

--- CD/ContentView.swift
+++ CD/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world world!")
+            Text("Hello, world world world!")
         }
         .padding()
     }
