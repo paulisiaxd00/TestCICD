ee8e002cca7ba17b5db1036a0ac0bac138598e6d
Paulina <paulisiaxd00@gmail.com>
2025-05-26 10:19:42 +1000

    rebase

--- CD/ContentView.swift
+++ CD/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TextView(viewModel: TextViewModel())
-        Button("Marta", action: {})
+        Button("Marta rebase", action: {})
     }
 }
 
