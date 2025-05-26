b09c0562745c358f4b7e03ea6b3256e701065a19
Paulina <paulisiaxd00@gmail.com>
2025-05-26 10:21:48 +1000

    Squash

--- CD/ContentView.swift
+++ CD/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         TextView(viewModel: TextViewModel())
-        Button("Marta rebase", action: {})
+        Button("Marta squash", action: {})
     }
 }
 
